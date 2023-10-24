7520d7cf49e7c5f63431f25fb87a1d15447f1846
samuel.behrmann@yahoo.de <samuel.behrmann@yahoo.de>
2023-10-24 11:33:18 +0200

    feat dynamic input

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject._
+import play.api._
 import play.api.mvc._
 import hearthstoneMini.model.Move
 import hearthstoneMini.controller.GameState
@@ -14,24 +15,31 @@ import hearthstoneMini.controller.Strategy
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   val controller = hearthstoneMini.HearthstoneMini.hearthstoneMiniRunner.controller
 
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.gameRules())
   }
 
-  def startGame = Action { implicit request: Request[AnyContent] =>
+  def game = Action { implicit request: Request[AnyContent] =>
     Ok(controller.gameState match {
-      case GameState.CHOOSEMODE => views.html.gamemode()
-      case GameState.ENTERPLAYERNAMES => views.html.index(tui = controller.field.toString())
-      case GameState.MAINGAME => views.html.index(tui = controller.field.toString())
-      case GameState.WIN => views.html.index(tui = controller.field.toString())
-      case GameState.EXIT => views.html.index(tui = controller.field.toString())
-    }
-    )
+                case GameState.CHOOSEMODE => views.html.gamemode()
+                case GameState.ENTERPLAYERNAMES => views.html.game(msg = controller.errorMsg.get)(field = controller.field.toString())
+                case GameState.MAINGAME => views.html.game(msg = controller.errorMsg.getOrElse(controller.field.players(0).name +  hearthstoneMini.aview.Strings.istDranMsg))(field= controller.field.toString())
+                case GameState.WIN => views.html.game(msg = "")(field= controller.field.toString())
+                case GameState.EXIT => views.html.game(msg = "")(field= controller.field.toString())
+            }
+      )
   }
 
-  def initGame() = Action { implicit request: Request[AnyContent] =>
-
-    controller.setStrategy(
+  def initGame() = Action {
+    implicit request: Request[AnyContent] => 
+      controller.setStrategy(
         (request.body.asFormUrlEncoded.get("gamemode").head).toInt match {
           case 1 => Strategy.normalStrategy()
           case 2 => Strategy.hardcoreStrategy()
@@ -39,55 +47,52 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
         }
       )
       controller.setPlayerNames(Move(p1 = request.body.asFormUrlEncoded.get("playerName1").head, p2 = request.body.asFormUrlEncoded.get("playerName2").head))
-      Redirect("/startGame")
+      Redirect("/hearthstoneMini")
   }
 
   def placeCard() = Action { 
-    controller.placeCard(hearthstoneMini.model.Move(1,1))
-    
     implicit request: Request[AnyContent] =>
-    Ok(views.html.index(tui= controller.field.toString()))
+      controller.placeCard(hearthstoneMini.model.Move(handSlot = request.body.asFormUrlEncoded.get("handSlotIndex").head.toInt, fieldSlotActive = request.body.asFormUrlEncoded.get("fieldIndex").head.toInt))
+      Redirect("/hearthstoneMini")
   }
   def exitGame() = Action { 
     controller.exitGame()
     
     implicit request: Request[AnyContent] =>
-    Ok(views.html.index(tui= controller.field.toString()))
+    Redirect("/hearthstoneMini")
   }
   def endTurn() = Action { 
     controller.switchPlayer()
     
     implicit request: Request[AnyContent] =>
-    Ok(views.html.index(tui= controller.field.toString()))
+    Redirect("/hearthstoneMini")
   }
   def drawCard() = Action { 
     controller.drawCard()
     
     implicit request: Request[AnyContent] =>
-    Ok(views.html.index(tui= controller.field.toString()))
+    Redirect("/hearthstoneMini")
   }
   def directAttack() = Action { 
-    controller.directAttack(hearthstoneMini.model.Move(1))
-    
     implicit request: Request[AnyContent] =>
-    Ok(views.html.index(tui= controller.field.toString()))
+      controller.directAttack(hearthstoneMini.model.Move(fieldSlotActive = request.body.asFormUrlEncoded.get("activeFieldIndex").head.toInt))
+      Redirect("/hearthstoneMini")
   }
-  def attack() = Action { 
-    controller.attack(hearthstoneMini.model.Move(1,1))
-    
+  def attack() = Action {
     implicit request: Request[AnyContent] =>
-    Ok(views.html.index(tui= controller.field.toString()))
+      controller.attack(hearthstoneMini.model.Move(handSlot = request.body.asFormUrlEncoded.get("activeFieldIndex").head.toInt, fieldSlotActive = request.body.asFormUrlEncoded.get("inactiveFieldIndex").head.toInt)) 
+      Redirect("/hearthstoneMini")
   }
   def undo() = Action { 
     controller.undo
     
     implicit request: Request[AnyContent] =>
-    Ok(views.html.index(tui= controller.field.toString()))
+    Redirect("/hearthstoneMini")
   }
   def redo() = Action { 
     controller.redo
     
     implicit request: Request[AnyContent] =>
-    Ok(views.html.index(tui= controller.field.toString()))
+    Redirect("/hearthstoneMini")
   }
 }
