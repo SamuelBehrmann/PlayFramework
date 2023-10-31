b945ecac312a988428e2268c3fb0e8a3eeebf53d
samuel.behrmann@yahoo.de <samuel.behrmann@yahoo.de>
2023-10-31 11:15:18 +0100

    feat: welcompage with buttons

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,31 +7,16 @@ import hearthstoneMini.model.Move
 import hearthstoneMini.controller.GameState
 import hearthstoneMini.controller.Strategy
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   val controller = hearthstoneMini.HearthstoneMini.hearthstoneMiniRunner.controller
-  /* 
-    controller.field.players.head.fieldbar.cardArea.row // Card/Field slots to map
-    controller.field.players.head.gamebar.hand // Hand karten 
-    controller.field.players.head.gamebar.deck // Deck 
-    controller.field.players.head.gamebar.friedhof // friedhof
-    controller.field.players.head.gamebar.hp
-    controller.field.players.head.gamebar.mana
-    */
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.welcome())
+  }
+
+  def gameRules = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.gameRules())
   }
 
   def game = Action { implicit request: Request[AnyContent] =>
