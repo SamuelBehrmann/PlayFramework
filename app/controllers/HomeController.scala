29f432c975ae1c2541da4bc76aef7ae5ed970418
Jan <janaminger@googlemail.com>
2023-11-19 14:46:01 +0100

    added decks + drawCard, removed Csrf Sicherheitskack etc.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -97,6 +97,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
         routes.javascript.HomeController.placeCard,
         routes.javascript.HomeController.attack,
         routes.javascript.HomeController.directAttack,
+        routes.javascript.HomeController.drawCard,
       )).as("text/javascript")
   }
 }
